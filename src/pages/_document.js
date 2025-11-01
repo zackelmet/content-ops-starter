8ecb9e657d749f1e8cdc9469dd7e49356b89d43a
Zack <zacharyelmetennani@gmail.com>
2025-11-01 14:29:54 -0500

    add google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,15 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head>
+                        <Head>
+                            {/* Google tag (gtag.js) */}
+                            <script async src="https://www.googletagmanager.com/gtag/js?id=G-QQQ9L6CW7D"></script>
+                            <script dangerouslySetInnerHTML={{__html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-QQQ9L6CW7D');
+                            `}} />
                     {/* Preload the local font for better performance */}
                     <link
                         rel="preload"
