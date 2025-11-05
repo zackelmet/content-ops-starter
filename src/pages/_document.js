ba892b856ae8ab46116200773f0ba84ea75e3667
Zack <zacharyelmetennani@gmail.com>
2025-11-05 11:27:01 -0500

    remove email subscription form from SignupBox and add MailerLite script to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,15 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                        <Head>
+                                <Head>
+                                    {/* MailerLite Universal */}
+                                    <script dangerouslySetInnerHTML={{__html: `
+                                        (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+                                        .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+                                        n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+                                        (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+                                        ml('account', '1823203');
+                                    `}} />
                             {/* Google tag (gtag.js) */}
                             <script async src="https://www.googletagmanager.com/gtag/js?id=G-QQQ9L6CW7D"></script>
                             <script dangerouslySetInnerHTML={{__html: `
