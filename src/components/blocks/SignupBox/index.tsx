c1625bf469f7e0570b3ba1bfa72dc6198ae67f44
Zack <zacharyelmetennani@gmail.com>
2025-11-05 21:05:50 -0500

    update SignupBox to render conditionally on the client side and disable SSR

--- src/components/blocks/SignupBox/index.tsx
+++ src/components/blocks/SignupBox/index.tsx
@@ -1,14 +1,21 @@
+
 import * as React from 'react';
 import classNames from 'classnames';
 
 export default function SignupBox(props) {
     const { className } = props;
+    const [isClient, setIsClient] = React.useState(false);
+
+    React.useEffect(() => {
+        setIsClient(true);
+    }, []);
+
     return (
         <div className={classNames('bg-white/5 backdrop-blur-lg rounded-xl p-6 shadow-lg w-full', className)}>
             <h3 className="text-xl font-semibold mb-2">Get Threat Alerts</h3>
             <p className="text-sm mb-4">Sign up for email alerts on new intelligence reports.</p>
             {/* MailerLite Embedded Form */}
-            <div className="ml-embedded" data-form="JJy1nd"></div>
+            {isClient && <div className="ml-embedded" data-form="JJy1nd"></div>}
         </div>
     );
 }
