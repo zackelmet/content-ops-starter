d0c0fcc0acf67d7ae5d2048871f4990ae0b14fb3
Zack <zacharyelmetennani@gmail.com>
2025-11-22 12:13:56 -0500

    Refactor Firebase configuration to use environment variables for sensitive data

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvbOFN9SMRQ82D6Q_DoDAZwLnXfwzM7Os",
-  authDomain: "hosted-scanners-30b84.firebaseapp.com",
-  projectId: "hosted-scanners-30b84",
-  storageBucket: "hosted-scanners-30b84.firebasestorage.app",
-  messagingSenderId: "635858390506",
-  appId: "1:635858390506:web:62f918faf4f9143cdf7693",
-  measurementId: "G-7107KHBD8G"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
